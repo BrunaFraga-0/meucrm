a91c167731fce1cd9e09fad554af19c2db09e14d
Bruna Fraga <brunafraga90@gmail.com>
2026-08-01 09:16:38 -0300

    feat: adiciona migration inicial de clientes

---
+++ backend/src/database/data-source.ts
@@ -0,0 +1,19 @@
+import * as dotenv from 'dotenv';
+import { DataSource } from "typeorm";
+
+dotenv.config();
+
+export const AppDataSource = new DataSource({
+    type: 'postgres',
+    host: process.env.PG_HOST,
+    port: Number(process.env.PG_PORT),
+    username: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
+    entities: ['src/**/*.entity{.ts,.js}'],
+    migrations: ['src/database/migrations/*{.ts,.js}'],
+    synchronize: false,
+    logging: true,
+});
+
+ 
---
+++ backend/src/database/migrations/1785584950587-CreateClientesTable.ts
@@ -0,0 +1,26 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateClientesTable1785584950587 implements MigrationInterface {
+    name = 'CreateClientesTable1785584950587'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            CREATE TABLE "clientes" (
+                    "id" uuid NOT NULL DEFAULT uuid_generate_v4(), 
+                    "nome" character varying(100) NOT NULL, 
+                    "email" character varying(255) NOT NULL, 
+                    "telefone" character varying(20) NOT NULL, 
+                    "empresa" character varying(100), 
+                    "observacoes" text, 
+                    "criado_em" TIMESTAMP NOT NULL DEFAULT now(), 
+
+                    CONSTRAINT "UQ_3cd5652ab34ca1a0a2c7a255313" UNIQUE ("email"), 
+                    CONSTRAINT "PK_d76bf3571d906e4e86470482c08" PRIMARY KEY ("id")
+                    )`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "clientes"`);
+    }
+
+}
